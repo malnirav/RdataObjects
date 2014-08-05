2f58b5a4a2a9d772509644143de691bb2330b14f
Nirav Malani <malnirav@gmail.com>
2014-08-05 17:02:48 -0400

    initial import

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-RdataObjects
+Misc R data objects + scripts
 ============
 
-Misc R data objects to be used for tutorials
+R data objects used for tutorials
 
 **genes**: UCSC refflat genes from hg38: http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/refFlat.txt.gz
 
 **sites**: HIV integration site data processed from PMID: 24489369 & 21561906
+
+-----
+
+R script used for report making tutorial
+
+**markdown_custom.R**: template R script to demonstrate usage of markdown\_custom.css
+**markdown_custom.Rmd**: template markdown document 
+**markdown_custom.css**: gutted bootstrap css with table of content mod for markdown reports
+
+-----
+
+**hiReadsProcessor_object.png**: structural hierarchy of sampleInfo object from hiReadsProcessor R package
---
+++ markdown_custom.R
@@ -0,0 +1,26 @@
+#### Load libraries ####
+libs <- c('knitr','markdown')
+sapply(libs, require, character.only=TRUE)
+
+#### External JS libraries for HTML header ####
+jsInclude <- c("http://code.jquery.com/jquery-1.10.2.min.js", 
+               "http://cdn.datatables.net/1.10.0/js/jquery.dataTables.js", 
+               "http://cdn.datatables.net/plug-ins/e9421181788/integration/bootstrap/3/dataTables.bootstrap.js")
+cssInclude <- c("http://cdn.datatables.net/plug-ins/e9421181788/integration/bootstrap/3/dataTables.bootstrap.css")
+freeText <- "<script>$(document).ready(function() { $('#datTable').dataTable();});</script>"
+headers <- paste(sapply(jsInclude, 
+                        function(x) sprintf("<script src='%s'></script>",x)),
+                 sapply(cssInclude, 
+                        function(x) 
+                            sprintf("<link rel='stylesheet' type='text/css' href='%s'>",x)),
+                 freeText,
+                 collapse=" ")
+
+#### make report ####
+filename <- "markdown_custom.md"
+knit("markdown_custom.Rmd", output = filename)
+
+markdownToHTML(filename, paste0(filename,".html"), extensions=c('tables'),
+               options=c(markdownHTMLOptions(defaults=TRUE),"toc"),
+               header=headers, stylesheet="markdown_custom.css")
+file.remove(filename)
